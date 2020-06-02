212376ba7a9e9962917ec2daf0a2a23801c96d52
RiiGrey <riiamri23@gmail.com>
2020-06-02 09:38:10 +0700

    creating migration, model, controller

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\model\Like;
+use Faker\Generator as Faker;
+
+$factory->define(Like::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
