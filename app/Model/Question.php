0328f9a4d38c42957e5052a0cbe2034f9b82e331
RiiGrey <riiamri23@gmail.com>
2020-06-03 22:20:11 +0700

    CRUD for Question

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,8 +3,34 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Model\Reply;
+use App\Model\Category;
 
 class Question extends Model
 {
-    //
+
+    // protected $fillable = ['title', 'slug', 'body', 'category_id', 'user_id'];
+
+    protected $guarded = [];
+
+    public function getRouteKeyName(){
+        return 'slug';
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies(){
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute(){
+        return asset('api/question/' . $this->slug);
+    }
 }
--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,11 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\model\Category;
+use App\Model\Category;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $word = $faker->word;
     return [
-        //
+        'name'=>$word,
+         'slug'=> Str::slug($word)
     ];
 });
